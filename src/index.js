ccf79c7b6ad359871b8b386e411273e765e81646
aaronjin <83966452@qq.com>
2016-04-01 15:54:25 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var ReactDOM = require("react-dom");
+var Hello_1 = require("./components/Hello");
+ReactDOM.render(<Hello_1.HelloComponent compiler="TypeScript" framework="React"/>, document.getElementById("example"));
+//# sourceMappingURL=index.js.map
